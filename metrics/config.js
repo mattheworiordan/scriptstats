f266927c035ddad41b47972ca2a9d19ba6c951a0
Steven Wilkin <stevenwilkin@gmail.com>
2013-11-18 09:53:20 +0000

    Remove duplicate requires

--- metrics/config.js
+++ metrics/config.js
@@ -1,7 +1,5 @@
 var redis = require('redis'),
     url = require('url'),
-    redis = require('redis'),
-    url = require('url'),
     redisURL = url.parse(process.env.REDISCLOUD_URL || 'redis://localhost:6379');
 
 exports.redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
